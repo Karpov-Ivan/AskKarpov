4a429be07c298ad1a8f432b1ca0976332ddb0e7c
Иван Карпов <ivankarpov03@gmail.com>
2023-11-03 14:50:55 +0300

    Homework 2

--- AskKarpov/app/views.py
+++ AskKarpov/app/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.core.paginator import Paginator
 
 QUESTIONS = [
@@ -10,34 +9,40 @@ QUESTIONS = [
     } for i in range(20)
 ]
 
-ANSWER = [
+ANSWERS = [
     {
         'id': i,
         'content': f'Long lorem ipsum {i}'
-    } for i in range(5)
+    } for i in range(15)
 ]
 
-def paginate(objects, page, per_page=10):
+
+def paginate(request, objects, per_page=5):
+    page_number = request.GET.get('page')
     paginator = Paginator(objects, per_page)
 
-    return paginator.page(page)
+    try:
+        page = paginator.get_page(page_number)
+    except EmptyPage:
+        page = paginator.get_page(1)
 
-# Create your views here.
+    return page
+
 def index(request):
-    page = request.GET.get('page', 1)
-    return render(request, template_name='index.html', context={'questions': paginate(QUESTIONS, page)})
+    return render(request, template_name='index.html', context={'questions': paginate(request, QUESTIONS)})
+
 
 def question(request, question_id):
     item = QUESTIONS[question_id]
-    return render(request, template_name='question.html', context={'question': item, 'answers': ANSWER})
+    return render(request, template_name='question.html',
+                  context={'question': item, 'questions': paginate(request, ANSWERS)})
 
 def hot_question(request):
-    page = request.GET.get('page', 1)
-    return render(request, template_name='hot-question.html', context={'questions': paginate(QUESTIONS, page)})
+    return render(request, template_name='hot-question.html', context={'questions': paginate(request, QUESTIONS)})
 
 def tag(request, tag_name):
-    page = request.GET.get('page', 1)
-    return render(request, template_name='tag.html', context={'questions': paginate(QUESTIONS, page), 'tags': tag_name})
+    return render(request, template_name='tag.html',
+                  context={'questions': paginate(request, QUESTIONS), 'tags': tag_name})
 
 def login(request):
     return render(request, template_name='login.html')
